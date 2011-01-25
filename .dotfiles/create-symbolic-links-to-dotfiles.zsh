0c6ad2ea9b9b0e3faa384d55d077dc9ff4593c1c
mt3 <na@na.na>
2011-01-25 10:33:47 -0800

    added create-symbolic-links-to-dotfiles.zsh to repo

---
+++ .dotfiles/create-symbolic-links-to-dotfiles.zsh
@@ -0,0 +1,30 @@
+# vim
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+ln -s ~/.dotfiles/vim/gvimrc ~/.gvimrc
+
+# git
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+ln -s ~/.dotfiles/gitignore_global ~/.gitignore_global
+
+ln -s ~/.dotfiles/hgrc ~/.hgrc
+
+ln -s ~/.dotfiles/htoprc ~/.htoprc
+
+ln -s ~/.dotfiles/fossil ~/.fossil
+
+ln -s ~/.dotfiles/emacs ~/.emacs
+
+ln -s ~/.dotfiles/zshrc ~/.zshrc
+
+ln -s ~/.dotfiles/screenrc ~/.screenrc
+
+ln -s ~/.dotfiles/muttrc ~/.muttrc
+
+ln -s ~/.dotfiles/mozconfig ~/.mozconfig
+
+ln -s ~/.dotfiles/gemrc ~/.gemrc
+
+ln -s ~/.dotfiles/conkerorrc ~/.conkerorrc
+
+ln -s ~/.dotfiles/ackrc ~/.ackrc
+
