8c394cbaf152d2f469ffe69c40095d8b628025de
n am <na@na.na>
2012-06-16 18:01:57 -0700

    Added many zsh aliases.
    * also cleaned formatting of aliases.zsh

--- .dotfiles/zsh/aliases.zsh
+++ .dotfiles/zsh/aliases.zsh
@@ -1,6 +1,4 @@
-# ZSH Aliases
-#############
-
+# ZSH Aliases #
 
 # APPLICATIONS {{{1
 #############################################################
@@ -10,14 +8,15 @@ alias jek="jekyll && echo && echo BROWSE TO: http://0.0.0.0:4000 && echo && jeky
 alias firefox='/Applications/BrowserApps/Nightly.app/Contents/MacOS/firefox'
 alias gclient="/Applications/github_dwlds/depot_tools/gclient"
 alias nightly='/Applications/BrowserApps/Nightly.app/Contents/MacOS/firefox'
+alias proxy='squid -f ~/etc/squid.conf && echo "Squid started."'
 # use my dev version of showoff
 alias showoff='/Applications/github_dwlds/showoff/bin/showoff'
 alias skim='/Applications/PDFApps/Skim.app/Contents/MacOS/Skim'
 alias spotoff="sudo mdutil -a -i off" # Disable Spotlight
 alias spoton="sudo mdutil -a -i on" # Enable Spotlight
 alias youtube-dl="/Applications/github_dwlds/youtube-dl/youtube-dl"
+alias v='vagrant'
 # }}}
-
 
 # GENERAL TERMINAL SHORTCUTS {{{1
 #############################################################
@@ -25,11 +24,25 @@ alias .='pwd'
 alias ..="cd .."
 alias ...="cd .. && cd .."
 alias ....="cd .. & cd .. & cd .."
-alias -g G='|grep'
+
+# Inline aliases, zsh -g aliases can be anywhere in command line
+alias -g G='| grep -'
+alias -g L='| less'
+#this reduces a command like:
+# ls | grep foo | less
+#to:
+# ls G foo L
 #TODO: toggle those suffix options that are wanted
+# alias -g R=' > /c/aaa/tee.txt '           # redirect
+# alias -g T=' | tee /c/aaa/tee.txt '       # tee
+# alias -g F=' | fmt -'                     # format
+# alias -g W=' | wc -l'                     # wc
 # alias -g L='|less'
 # alias -g W='|wc'
 # alias -g C='|colordiff'
+# alias -g ONE="| awk '{ print \$1}'"
+
+alias ack='ack -a'
 alias apps="cd /Applications"
 alias appsg="cd /Applications/github_dwlds"
 alias cdd='cd -' # goto last dir cd'ed from
@@ -37,6 +50,7 @@ alias ciao='sudo shutdown -h now'
 alias clr="clear"
 alias curl="curl -O"
 alias dir_sizes='du -cks * | sort -rn |head -11' # Lists the size of all the folders within current dir (Mb)
+alias disks='diskutil list'
 alias dotf="ls .[a-zA-Z0-9_]*"
 alias down="cd ~/Downloads"
 alias du='du -h -d 2'
@@ -66,6 +80,10 @@ alias lls="ls -Gh"
 alias ls='ls -alGh'
 alias lsd='ls -ld *(-/DN)' # list only dirs
 # alias lsd='ls -l | grep "^d"' # List only directories
+#list all files without an extension (no dot)
+alias lsnoext='ls *~*.*(.)'
+# print the 5 most recent files
+alias lsrecent5='ls *(om[1,5])'
 alias lss='ls --color=auto --human-readable --group-directories-first --classify'
 alias md="mkcd"
 alias modified_today='ls *(m-1)' # files modified today. or could use 'ls *(m0)'
@@ -86,13 +104,14 @@ alias ssh="ssh -c arcfour,blowfish-cbc -C"
 # This trick makes sudo understand all my aliases
 alias sudo='sudo '
 #alias systail='tail -f -n0 /var/log/system.log'
+alias tlf='tail -f'
 alias TODOs="ack 'TODO|FIXME|XXX|HACK'"
 alias trash="rm -rf"
 alias up="cd .."
 alias upd="upd >&1 > ~/.logs/updated-repos-logfiles-`date +%F`-`date +%T`.txt"
+alias whats-my-ip="curl -s checkip.dyndns.org | grep -Eo '[0-9\.]+'"
 alias zshrc='mvim ~/.zshrc && source ~/.zshrc' # opens zshrc file in textmate, then reloads it immediately (thieved from http://ozmm.org/posts/git_bash_aliases.html)
 # }}}
-
 
 # VIM {{{1
 #############################################################
@@ -102,13 +121,11 @@ alias vim='/Applications/DevApps/MacVim.app/Contents/MacOS/Vim'
 alias vimdiff='/usr/local/bin/mvim mvimdiff'
 # }}}
 
-
 # SOLR {{{1
 #############################################################
 alias solrstart="sh /Applications/LucidImagination/LucidWorksEnterprise/app/bin/start.sh"
 alias solrstop="sh /Applications/LucidImagination/LucidWorksEnterprise/app/bin/stop.sh"
 # }}}
-
 
 # TMUX {{{1
 #############################################################
@@ -117,7 +134,6 @@ alias tmuxls="tmux ls"
 # for tmux integration with iterm2
 alias tmux='tmux -C'
 # }}}
-
 
 # TASK {{{1
 #############################################################
@@ -128,7 +144,6 @@ alias taskd="task done"
 alias taskc="task completed"
 alias taskh="task ghistory"
 # }}}
-
 
 # CABAL (haskell) {{{1
 #############################################################
@@ -143,12 +158,10 @@ alias cabu="cabal install"
 #alias cabc="cabal clean" TODO
 # }}}
 
-
 # FABRIC {{{1
 #############################################################
 alias fabl="fab --list-format=nested --list"
 # }}}
-
 
 # GIT {{{1
 #############################################################
@@ -174,7 +187,7 @@ alias gitsub="git submodule foreach 'git pull'"
 # GIT FLOW {{{2
 alias gf="git flow"
 # }}}
-
+# }}}
 
 # HOMEBREW {{{1
 #############################################################
@@ -188,7 +201,6 @@ alias bo="brew outdated"
 alias brewc="brew cleanup && cd /Users/mt/Library/Caches/Homebrew/ && for item in *; do rm -rf $item; done"
 # }}}
 
-
 # RUBY GEMS {{{1
 #############################################################
 alias gemu="gem update"
@@ -200,15 +212,19 @@ alias gems="gem search -r"
 alias gemo="gem outdated"
 # }}}
 
-
 # PYTHON {{{1
 #############################################################
+alias p="python"
 alias py="python"
 alias pyv="python -V"
 
+alias pipf="pip freeze"
 alias pips="pip search"
-alias pipl="pip freeze"
+# alias pipl="pip freeze"
+alias pipl="yolk -l"
 alias pipin="pip install"
+alias pipnew="yolk --latest-releases 24"
+alias pipr="pip freeze"
 # alias pipup="pip install --upgrade"
 # update all outdated packages
 alias pipup="pip freeze --local | cut -d = -f 1  | xargs pip install -U"
@@ -223,15 +239,13 @@ alias piprecent="yolk -L 12"
 alias stallion="python -m stallion.main"
 # }}}
 
-
 # LUCENE {{{1
 #############################################################
 alias lucin="java org.apache.lucene.demo.IndexFiles"
 alias lucs="java org.apache.lucene.demo.SearchFiles"
 # }}}
 
-
-# NODE and NPM {{{1
+# NODE & NPM {{{1
 #############################################################
 alias npmin="npm install"
 alias npml="npm ls installed"
@@ -248,14 +262,12 @@ alias npmcl="npm cache clean"
 alias npmi="npm view"
 # }}}
 
-
-# Suffix Aliases {{{1
+# SUFFIX ALIASES {{{1
 #############################################################
 alias -s tex=mvim # open all tex files with vim
 alias -s org=mvim
 alias -s pdf=skim
 # }}}
-
 
 echo "\e[1m\e[32mFinished loading aliases.zsh\e[0m"
 
