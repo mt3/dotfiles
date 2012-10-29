f51f3ec0d35bedd5400fd10aef8596090edfaf1e
n am <na@na.na>
2012-10-29 10:37:08 -0700

    Added zsh shell variables.

--- .dotfiles/zsh/vars.zsh
+++ .dotfiles/zsh/vars.zsh
@@ -1,38 +1,48 @@
 # ZSH Variables
 ###############
 
-export OH_MY_ZSH_DEBUG="true"
-# don't put duplicate lines in history
-# & don't overwrite Midnight Commander's `ignorespace' setting
-# http://github.com/matflores/dotfiles/blob/master/bashrc
-HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
-# ... or force ignoredups and ignorespace
-# HISTCONTROL=ignoreboth
-HISTCONTROL=eraseboth
-#HISTFILE=~/.history
-HISTSIZE=10500
-SAVEHIST=10000
-#setopt SHARE_HISTORY
-setopt EXTENDED_HISTORY
-setopt HIST_VERIFY
-setopt HIST_APPEND
-setopt HIST_IGNORE_DUPS
-#setopt HIST_IGNORE_ALL_DUPS #i'm bypassing this since it messes with recent history/chronology (doesn't retain recent occurences)
-setopt HIST_REDUCE_BLANKS
-setopt HIST_IGNORE_SPACE
+# zsh {{{
+    export OH_MY_ZSH_DEBUG="true"
+    # don't put duplicate lines in history
+    # & don't overwrite Midnight Commander's `ignorespace' setting
+    # http://github.com/matflores/dotfiles/blob/master/bashrc
+    HISTCONTROL=$HISTCONTROL${HISTCONTROL+,}ignoredups
+    # ... or force ignoredups and ignorespace
+    # HISTCONTROL=ignoreboth
+    HISTCONTROL=eraseboth
+    #HISTFILE=~/.history
+    HISTSIZE=10500
+    SAVEHIST=10000
+    #setopt SHARE_HISTORY
+    setopt EXTENDED_HISTORY
+    setopt HIST_VERIFY
+    setopt HIST_APPEND
+    setopt HIST_IGNORE_DUPS
+    #setopt HIST_IGNORE_ALL_DUPS #i'm bypassing this since it messes with recent history/chronology (doesn't retain recent occurences)
+    setopt HIST_REDUCE_BLANKS
+    setopt HIST_IGNORE_SPACE
 
-# red dots to be displayed while waiting for completion
-COMPLETION_WAITING_DOTS="true"
+    REPORTTIME=60 # Report time statistics for progs that take more than a minute to run
+    WATCHFMT='%n %a %l from %m at %T.'
 
-# avoid completion when "zsh: correct 'vim' to '.vim' [nyae]? n"
-CORRECT_IGNORE=".*"
-#CORRECT_IGNORE=".vim"
+    # red dots to be displayed while waiting for completion
+    COMPLETION_WAITING_DOTS="true"
 
-# M-b and M-f (backward-word and forward-word) doesn't jump over an entire /path/location, but moves to each word separated by a '/'
-export WORDCHARS=''
-# by default: export WORDCHARS='*?_-.[]~=/&;!#$%^(){}<>'
-# we take out the slash, period, angle brackets, dash here.
-# export WORDCHARS='*?_[]~=&;!#$%^(){}'
+    # avoid completion when "zsh: correct 'vim' to '.vim' [nyae]? n"
+    CORRECT_IGNORE=".*"
+    #CORRECT_IGNORE=".vim"
+
+    # M-b and M-f (backward-word and forward-word) doesn't jump over an entire /path/location, but moves to each word separated by a '/'
+    export WORDCHARS=''
+    # by default: export WORDCHARS='*?_-.[]~=/&;!#$%^(){}<>'
+    # we take out the slash, period, angle brackets, dash here.
+    # export WORDCHARS='*?_[]~=&;!#$%^(){}'
+
+    export ZSH=$HOME/code_and_projects/dotfiles/.dotfiles/zsh/oh-my-zsh
+    # export ZSH_THEME="clean"
+    export ZSH_THEME="powerline"
+    export TERM="xterm-256color"
+# }}}
 
 # auto-launch ssh keypairs so i don't have to enter the damned 137.5 character pwd every time
 export SSH_ENV="$HOME/.ssh/environment"
@@ -50,19 +60,20 @@ export LSCOLORS=Exfxcxdxbxegedabagacad
 # Don’t clear the screen after quitting a manual page
 export MANPAGER='less -X'
 
-# JAVA
-# export java classpath for lucene
-export CLASSPATH=/usr/local/Cellar/lucene-3.0.2/lucene-core-3.0.2.jar:/usr/local/Cellar/lucene-3.0.2/lucene-demos-3.0.2.jar:/Volumes/Data/incoming/stanford-parser-2010-02-26/stanford-parser.jar
-export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
+# JAVA {{{
+    # export java classpath for lucene
+    export CLASSPATH=/usr/local/Cellar/lucene-3.0.2/lucene-core-3.0.2.jar:/usr/local/Cellar/lucene-3.0.2/lucene-demos-3.0.2.jar:/Volumes/Data/incoming/stanford-parser-2010-02-26/stanford-parser.jar
+    export JAVA_HOME=/System/Library/Java/JavaVirtualMachines/1.6.0.jdk/Contents/Home
 
-# add Stanford parser to path
-export STANFORD_PATH=/usr/local/Cellar/stanford-parser/1.6.2
-export PATH=$PATH:$STANFORD_PATH
+    # add Stanford parser to path
+    export STANFORD_PATH=/usr/local/Cellar/stanford-parser/1.6.2
+    export PATH=$PATH:$STANFORD_PATH
 
-#for i in web util trees swing stats process parser objectbank movetrees misc math ling io international fsm
-#do
-#  export PATH=$PATH:$STANFORD_PATH/src/edu/stanford/nlp/$i
-#done
+    #for i in web util trees swing stats process parser objectbank movetrees misc math ling io international fsm
+    #do
+    #  export PATH=$PATH:$STANFORD_PATH/src/edu/stanford/nlp/$i
+    #done
+# }}}
 
 # NODE & NPM
 export NODE_PATH=/usr/local/lib/node:/usr/local/lib/node_modules
@@ -71,29 +82,32 @@ export PATH=$PATH:/usr/local/share/npm/bin
 # LATEX & BIBTEX
 export PATH=$PATH:/usr/texbin
 
-# PYTHON
-# place python packages on path
-#export PATH=$PATH:/usr/local/share/python:/usr/local/python/site-packages:/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages:/Library/Python/2.6/site-packages:/Library/Frameworks/Python.framework/Versions/2.6/bin
-export PATH=$PATH:/usr/local/share/python:/usr/local/python/site-packages:/usr/local/lib/python2.7/site-packages
+# PYTHON {{{
+    # place python packages on path
+    #export PATH=$PATH:/usr/local/share/python:/usr/local/python/site-packages:/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages:/Library/Python/2.6/site-packages:/Library/Frameworks/Python.framework/Versions/2.6/bin
+    export PATH=$PATH:/usr/local/share/python:/usr/local/python/site-packages:/usr/local/lib/python2.7/site-packages
 
-#export PYTHONPATH=/usr/local/share/python:/usr/local/python/site-packages:/Library/Frameworks/Python.framework/Versions/2.6/bin:/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages:/usr/local/lib/python2.6/site-packages/
-export PYTHONPATH=/usr/local/share/python:/usr/local/python/site-packages:/usr/local/lib/python2.7/site-packages:/usr/local/Cellar/python/2.7.2/lib/python2.7
-# place fabric files on pythonpath
-export PYTHONPATH=$PYTHONPATH:/Users/mt/code_and_projects/sysadmin-and-deployment/fabfiles
+    #export PYTHONPATH=/usr/local/share/python:/usr/local/python/site-packages:/Library/Frameworks/Python.framework/Versions/2.6/bin:/Library/Frameworks/Python.framework/Versions/2.6/lib/python2.6/site-packages:/usr/local/lib/python2.6/site-packages/
+    export PYTHONPATH=/usr/local/share/python:/usr/local/python/site-packages:/usr/local/lib/python2.7/site-packages:/usr/local/Cellar/python/2.7.2/lib/python2.7
+    # place fabric files on pythonpath
+    export PYTHONPATH=$PYTHONPATH:/Users/mt/code_and_projects/sysadmin-and-deployment/fabfiles
 
-# virtualenv
-export WORKON_HOME=$HOME/.virtualenvs
-export PROJECT_HOME=$HOME/virtualenv-devel
-export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
-export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/share/python/virtualenvwrapper.sh
-# make pip use the same directory for virtualenvs as virtualenvwrapper
-export PIP_VIRTUALENV_BASE=$WORKON_HOME
-# export PIP_RESPECT_VIRTUALENV=true
+    # virtualenv
+    export WORKON_HOME=$HOME/.virtualenvs
+    export PROJECT_HOME=$HOME/virtualenv-devel
+    source /usr/local/bin/virtualenvwrapper.sh
+    export VIRTUALENVWRAPPER_PYTHON=/usr/local/bin/python
+    export VIRTUALENVWRAPPER_VIRTUALENV=/usr/local/share/python/virtualenvwrapper.sh
+    # make pip use the same directory for virtualenvs as virtualenvwrapper
+    export PIP_VIRTUALENV_BASE=$WORKON_HOME
+    # export PIP_RESPECT_VIRTUALENV=true
 
-export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
+    # pip
+    export PIP_DOWNLOAD_CACHE="$HOME/.pip/cache"
 
-# override python default of 64-bit mode
-export VERSIONER_PYTHON_PREFER_32_BIT=no
+    # override python default of 64-bit mode
+    export VERSIONER_PYTHON_PREFER_32_BIT=no
+# }}}
 
 # some more customization for Lion
 # TODO: is this needed anymore?...
@@ -151,12 +165,6 @@ export VIM_APP_DIR=/Applications/DevApps
 # place ccache on head of path for faster xcode compiling
 #export PATH=/usr/local/Cellar/ccache/3.1.4/libexec:$PATH
 
-# ZSH
-export ZSH=$HOME/code_and_projects/dotfiles/.dotfiles/zsh/oh-my-zsh
-# export ZSH_THEME="clean"
-export ZSH_THEME="powerline"
-export TERM="xterm-256color"
-
 # ADD gsl LIBRARY TO THE PATH
 export PATH=/usr/local/Cellar/gsl/1.14/lib:/usr/local/Cellar/gsl/1.14/include:$PATH
 
@@ -168,7 +176,7 @@ export PATH=/usr/local/Cellar/gsl/1.14/lib:/usr/local/Cellar/gsl/1.14/include:$P
 
 export MACOSX_DEPLOYMENT_TARGET=10.8
 
-# For cydia/jailbreak apps
+# cydia/jailbreak apps
 export THEOS=/Applications/github_dwlds/theos
 
 # STAGE env variables
